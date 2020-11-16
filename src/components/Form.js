47eba7d9caa9a43a3295295d729f68694149884b
Maxim Bielushkin <belushkin@gmail.com>
2020-11-16 21:09:23 +0200

    Added new function instead of class

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { Component } from "react";
 import BodyWeightSlider from './BodyWeightSlider';
 import { 
@@ -7,53 +8,95 @@ import {
     FormLabel, 
     RadioGroup, 
     FormControlLabel,
+    InputLabel,
+    Select,
+    MenuItem,
   } from '@material-ui/core';
 
 import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
 
-export default class Form extends Component {
-  state = { 
-    gender: "male",
-    weight: 70
-  };
-  
-  handleChange = (event, value) => {
-    const name = event.target.name === undefined ? 'weight' : event.target.name;
-    this.setState({[name]: value});
-  };
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
-  render() {
-    const { gender, weight } = this.state;
+export default function Form() {
+
+  const [gender, setGender] = useState("male");
+  const [weight, setWeight] = useState(70);
+
     return (
-      <FormControl component="fieldset">
-        <FormLabel component="legend">Оберіть свою стать:</FormLabel>
-        <RadioGroup 
-          onChange={this.handleChange} 
-          value={gender}
-          name="gender"
-          row 
-          aria-label="gender" 
-        >
-        <FormControlLabel
-          value="male"
-          control={<Radio color="primary"/>}
-          label="Чоловік"
-        />
-        <FormControlLabel
-          value="female"
-          control={<Radio color="primary"/>}
-          label="Жінка"
-        />
-        </RadioGroup>
-        <Box component="span" mt={3}>
-          <BodyWeightSlider 
-            value={weight}
-            onChange={this.handleChange}
-          >
-          </BodyWeightSlider>
+      <form>
+        {/* ===========Radio group of sex selection===================== */}
+        <Box component="div">
+          <FormControl component="fieldset">
+            <FormLabel component="legend">Оберіть свою стать:</FormLabel>
+            <RadioGroup 
+              onChange={e => setGender(e.target.value)} 
+              value={gender}
+              name="gender"
+              row 
+              aria-label="gender" 
+            >
+            <FormControlLabel
+              value="male"
+              control={<Radio color="primary"/>}
+              label="Чоловік"
+            />
+            <FormControlLabel
+              value="female"
+              control={<Radio color="primary"/>}
+              label="Жінка"
+            />
+            </RadioGroup>
+          </FormControl>
         </Box>
-        <Button variant="contained" color="primary">Розрахувати</Button>
-      </FormControl>
+
+        {/* ===========Body weight slider===================== */}
+        <Box component="div" mt={3}>
+          <FormControl component="fieldset">
+            <BodyWeightSlider 
+              value={weight} 
+              onChange={(e, value) => setWeight(value)}
+            ></BodyWeightSlider>
+          </FormControl>
+        </Box>
+
+        {/* ===========Drink selection===================== */}
+        {/* <Box component="div" mt={3}>
+          <FormControl component="fieldset">
+            <InputLabel component="legend" id="select-drink-label">Напій</InputLabel>
+            <Select
+              labelId="select-drink-label"
+              id="select-drink"
+              // value={age}
+              // onChange={handleChange}
+            >
+              <MenuItem value={0.5}>"Безалкогольне" пиво (0.5%)</MenuItem>
+              <MenuItem value={0.7}>Кефір (0.7%)</MenuItem>
+              <MenuItem value={0.9}>Квас (0.9%)</MenuItem>
+            </Select>
+          </FormControl>
+        </Box> */}
+
+          {/* <Box component="span" mt={3}>
+            <BodyWeightSlider value={weight} onChange={this.handleChange}></BodyWeightSlider>
+          </Box>
+          <Box component="span" mt={3}>
+            
+          </Box>
+          <Button variant="contained" color="primary">Розрахувати</Button> */}
+        
+
+        
+
+      </form>
     );
   }
-}
+
