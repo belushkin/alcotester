3ae3c73878f18cbdbff4be4af247990ec0132bbf
Maxim Bielushkin <belushkin@gmail.com>
2020-12-01 17:12:54 +0200

    Added Google Analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-27R7HQMZQ8');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
